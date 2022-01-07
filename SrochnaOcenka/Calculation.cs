5a46adfa769be63509744886df574a23acb66388
dayofpay <42325421+dayofpay@users.noreply.github.com>
2022-01-07 02:47:15 +0200

    1.0

---
+++ SrochnaOcenka/Calculation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrochnaOcenka
+{
+    class Calculation
+    {
+        public static double final
+        {
+            get;
+            set;
+        }
+        public static void Calc(double gradeSum, double totalGrades)
+        {
+            final = gradeSum / totalGrades;
+            if(final > 6)
+            {
+                Console.WriteLine("Вашата оценка е: 6.00");
+                Console.WriteLine($"[DEBUG] Изчисление : {gradeSum} / {totalGrades}");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Натиснете бутон 'R' за да рестартирате програмата ...");
+                var resetKey = Console.ReadKey();
+                if(resetKey.Key == ConsoleKey.R)
+                {
+                    SrochnaOcenka.Restart.resetSoftware();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вашата оценка е: " + final);
+                Console.WriteLine($"[DEBUG] Изчисление : {gradeSum} / {totalGrades}");
+                Console.WriteLine("Натиснете бутон 'R' за да рестартирате програмата ...");
+                var resetKey = Console.ReadKey();
+                if (resetKey.Key == ConsoleKey.R)
+                {
+                    SrochnaOcenka.Restart.resetSoftware();
+                }
+            }
+        }
+    }
+}
---
+++ SrochnaOcenka/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SrochnaOcenka
+{
+    public class Restart
+    {
+        public static void resetSoftware()
+        {
+            Program.Main();
+        }
+    }
+    class Program
+    {
+        public static double totalOcenki
+        {
+            get;
+            set;
+        }
+        public static int totalInvalid
+        {
+            get;
+            set;
+        }
+        public static void Main()
+        {
+            Console.Title = "Срочна Оценка | https://github.com/dayofpay";
+            List<double> spisukOcenki = new List<double>();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Моля, посочете всички оценки със (,) : ");
+            var broiOcenki = Console.ReadLine();
+            String[] broi = broiOcenki.Split(new[] { ',' });
+            var realOcenki = broi.Length - 1;
+            totalOcenki = realOcenki;
+            for (int b = 0; b <= totalOcenki; b++)
+            {
+                if (int.Parse(broi[b]) > 6)
+                {
+                    Console.WriteLine("Това число беше игнорирано, тъй като е по-голямо от максималната оценка '6' [" + broi[b] + "]");
+                    totalInvalid++;
+                }
+                if (int.Parse(broi[b]) < 2) {
+                    Console.WriteLine("Това число беше игнорирано, тъй като е по-малко от минималната оценка '2' [" + broi[b] + "]");
+                    totalInvalid++;
+                }
+                else if (int.Parse(broi[b]) >= 2 && int.Parse(broi[b]) <= 6)
+                {
+                    Console.WriteLine($"Оценка №{b} : {broi[b]}");
+                    spisukOcenki.Add(int.Parse(broi[b]));
+                }
+            }
+            // Изчисление ...
+            if(totalInvalid > 0)
+            {
+                var sum = spisukOcenki.Sum();
+                Calculation.Calc(sum, broi.Length - totalInvalid);
+            }
+            else
+            {
+                var sum = spisukOcenki.Sum();
+                Calculation.Calc(sum, broi.Length);
+            }
+            // Изчисление ...
+        }
+    }
+}
